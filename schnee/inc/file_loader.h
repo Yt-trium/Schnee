e565936d3aa3804e7b2d8849e47a0f3fd81214e5
Kevin Masson <contact.kevinmasson@gmail.com>
2018-02-07 11:33:22 +0100

    Implement off points loader

---
+++ schnee/inc/file_loader.h
@@ -0,0 +1,11 @@
+#ifndef __FILE_LOADER__
+#define __FILE_LOADER__
+
+#include "vector.h"
+
+#include <string>
+#include <vector>
+
+void FL_OFF_load_points(const std::string &, std::vector<Vector3> &);
+
+#endif
---
+++ schnee/src/file_loader.cpp
@@ -0,0 +1,44 @@
+#include "file_loader.h"
+
+#include <fstream>
+#include <stdexcept>
+#include <iostream>
+
+void FL_OFF_load_points(const std::string & path, std::vector<Vector3> & output)
+{
+	std::ifstream reader(path);
+	// Checkup
+	if(!reader.is_open()) throw std::runtime_error("Unable to open the file");
+
+	// Reading variables
+	std::string reader_str;
+	int reader_int;
+	float reader_float;
+
+	// Check file type
+	reader >> reader_str;
+	if(!reader_str.compare("OFF"))
+	{
+		reader.close();
+		std::cout << path << " is not an off file" << std::endl;
+		return;
+	}
+
+	int vert_count;
+	reader >> vert_count >> reader_int >> reader_int; // Line ends with 0
+	std::cout << "Vert count: " << vert_count << std::endl;
+
+	// Read verts
+	float x, y, z;
+	for(int i = 0; i < vert_count; i++)
+	{
+		reader >> x >> y >> z;
+		output.emplace_back(x, y, z);
+	}
+
+	// Cleaning
+	reader.close();
+
+	return;
+}
+
